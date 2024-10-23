145be55df58ac4f0566cae4e10696d8ff505142a
ividernvi <ividernvi@outlook.com>
2024-10-23 09:17:13 +0800

    Support document union model

---
+++ pkg/model/v1/document.go
@@ -0,0 +1,16 @@
+package v1
+
+type Document struct {
+	ObjectMeta `json:"metadata,omitempty"`
+	DocumentId string       `json:"document_id,omitempty" gorm:"unique;column:documentId;type:varchar(20);not null"`
+	Title      string       `json:"title,omitempty" gorm:"column:title;type:varchar(30);not null"`
+	Authors    AuthorsUnion `json:"author,omitempty" gorm:"column:author;type:varchar(30);not null"`
+	Tags       TagsUnion    `json:"tags,omitempty" gorm:"column:author;type:varchar(30);not null"`
+
+	UpdatedUserId string `json:"user,omitempty" gorm:"colum:updatedUserId;type:varchar(20);not null"`
+}
+
+type DocumentList struct {
+	ListMeta `json:",inline"`
+	Items    []*Document `json:"items"`
+}
