cdf87ec937e56c569c8132a2c746e9f76755ce2f
ividernvi <ividernvi@outlook.com>
2024-11-04 18:42:23 +0800

    Add copyright message

--- pkg/model/v1/types.go
+++ pkg/model/v1/types.go
@@ -1,6 +1,13 @@
+// Copyright 2020 Lingfei Kong <colin404@foxmail.com>. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
 package v1
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type TypeMeta struct {
 	Kind       string `json:"kind,omitempty"`
@@ -8,11 +15,12 @@ type TypeMeta struct {
 }
 
 type ObjectMeta struct {
-	ID         uint64    `json:"id,omitempty" gorm:"primary_key;AUTO_INCREMENT;column:id"`
-	InstanceID string    `json:"instanceID,omitempty" gorm:"unique;column:instanceID;type:varchar(32);not null"`
-	Name       string    `json:"name,omitempty" gorm:"column:name;type:varchar(64);not null" validate:"name"`
-	CreatedAt  time.Time `json:"createdAt,omitempty" gorm:"column:createdAt"`
-	UpdatedAt  time.Time `json:"updatedAt,omitempty" gorm:"column:updatedAt"`
+	ID         uint64         `json:"id,omitempty" gorm:"primary_key;AUTO_INCREMENT;column:id"`
+	InstanceID string         `json:"instanceID,omitempty" gorm:"unique;column:instanceID;type:varchar(32);not null"`
+	Name       string         `json:"name,omitempty" gorm:"column:name;type:varchar(64);not null" validate:"name"`
+	CreatedAt  time.Time      `json:"createdAt,omitempty" gorm:"column:createdAt"`
+	UpdatedAt  time.Time      `json:"updatedAt,omitempty" gorm:"column:updatedAt"`
+	Deleted    gorm.DeletedAt `json:"deltedAt,omitempty" `
 }
 
 type ListMeta struct {
